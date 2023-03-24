297d22b4e42fa2bb61f9bc659f8a1d0fcd15cb47
mrbilby <baileymrb@gmail.com>
2023-03-24 07:25:37 +0000

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by James Bailey on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
